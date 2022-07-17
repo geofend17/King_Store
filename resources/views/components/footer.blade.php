56cf233e9945fa57206568eee2cd5e4df4faa0fe
geofend17 <geofend.vieri@gmail.com>
2022-07-17 23:40:02 +0700

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,58 @@
+ <!-- Footer -->
+    <footer class="footer section">
+      <div class="footer_container container grid">
+        <div class="footer_content">
+          <h3 class="footer_title">Our information</h3>
+          <ul class="footer_list">
+            <li>Jakarta, Indonesia</li>
+            <li>+62 123 4567 8910</li>
+          </ul>
+        </div>
+
+        <div class="footer_content">
+          <h3 class="footer_title">About Us</h3>
+
+          <ul class="footer_links">
+            <li><a href="#" class="footer_link">Support Center</a></li>
+            <li><a href="#" class="footer_link">Customer Service</a></li>
+            <li><a href="#" class="footer_link">Contact Us</a></li>
+            <li><a href="#" class="footer_link">Copy Right</a></li>
+          </ul>
+        </div>
+
+        <div class="footer_content">
+          <h3 class="footer_title">Product</h3>
+
+          <ul class="footer_links">
+            <li><a href="#" class="footer_link">Hoodie</a></li>
+            <li><a href="#" class="footer_link">Shirt</a></li>
+            <li><a href="#" class="footer_link">Sweater</a></li>
+            <li><a href="#" class="footer_link">Sneakers</a></li>
+          </ul>
+        </div>
+
+        <div class="footer_content">
+          <h3 class="footer_title">Social Media</h3>
+
+          <ul class="footer_social">
+            <a href="#" target="_blank" class="footer_social-link">
+              <i class="bx bxl-facebook"></i>
+            </a>
+
+            <a href="#" target="_blank" class="footer_social-link">
+              <i class="bx bxl-twitter"></i>
+            </a>
+
+            <a href="#" target="_blank" class="footer_social-link">
+              <i class="bx bxl-instagram"></i>
+            </a>
+
+            <a href="#" target="_blank" class="footer_social-link">
+              <i class="bx bxl-github"></i>
+            </a>
+          </ul>
+        </div>
+      </div>
+
+      <span class="footer_copy">&#169; Midnight Club. All rights reserved</span>
+    </footer>
